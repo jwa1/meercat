6c8306aec899bd72f5ec6b00ec36d29f3fc60552
Jamie Watts <jamiwatt@cisco.com>
2020-03-12 13:40:13 +1000

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+# /app.py
+
+from flask import Flask, request, jsonify, render_template
+import os
+import dialogflow
+import requests
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# run Flask app
+if __name__ == "__main__":
+    app.run()
