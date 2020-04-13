afc6fdc0d5aeb5a2a4e1d6f87e1e82499fbcaccd
Jamie Watts <jamiwatt@cisco.com>
2020-04-13 15:23:01 +1000

    Added licenses and cookie cutter template files

--- bot.py
+++ bot.py
@@ -1,7 +1,24 @@
+"""
+Copyright (c) 2020 Cisco and/or its affiliates.
+
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+
+               https://developer.cisco.com/docs/licenses
+
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+"""
+
+
 from flask import jsonify
 from webexteamssdk import WebexTeamsAPI, Webhook
 from webexteamssdk.exceptions import ApiError
-
 
 import json
 import os
@@ -232,10 +249,10 @@ class ChatBot():
         # Get the sender's details
         person = self.api.people.get(message.personId)
 
-        if __debug__:
-            print(f"\nNEW MESSAGE IN ROOM '{room.title}'")
-            print(f"FROM '{person.displayName}'")
-            print(f"MESSAGE '{message.text}'\n")
+        # if __debug__:
+        #     print(f"\nNEW MESSAGE IN ROOM '{room.title}'")
+        #     print(f"FROM '{person.displayName}'")
+        #     print(f"MESSAGE '{message.text}'\n")
 
         # This is a VERY IMPORTANT loop prevention control step.
         # If you respond to all messages...  You will respond to the messages
